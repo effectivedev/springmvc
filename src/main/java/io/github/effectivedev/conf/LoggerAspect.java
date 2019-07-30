0fce2acdef6ae8e65dfebfb6c0f1132e963dafae
Inyoung <in0@me.com>
2019-07-30 14:53:17 +0900

    log4j2 slf4j 설정 추가

---
+++ src/main/java/io/github/effectivedev/conf/LoggerAspect.java
@@ -0,0 +1,9 @@
+package io.github.effectivedev.conf;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+public class LoggerAspect {
+
+}
--- src/main/java/io/github/effectivedev/controller/TestController.java
+++ src/main/java/io/github/effectivedev/controller/TestController.java
@@ -1,21 +1,27 @@
 package io.github.effectivedev.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.test.context.transaction.TestContextTransactionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
-public class TestController{
+public class TestController {
 
     @GetMapping("/test")
     @ResponseBody
-    public String test(){
+    public String test() {
+        log.info("Test3");
         return "Hello World";
     }
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 }
