6693ac489ad633ac9af2105d9aab296fc762100a
Inyoung <in0@me.com>
2019-07-30 15:27:00 +0900

    add aop settings and delete lib

--- src/main/java/io/github/effectivedev/conf/LoggerAspect.java
+++ src/main/java/io/github/effectivedev/conf/LoggerAspect.java
@@ -1,9 +1,29 @@
 package io.github.effectivedev.conf;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
 
+@Component
 @Aspect
+@EnableAspectJAutoProxy
 public class LoggerAspect {
+    @Around("execution(* io.github.effectivedev.controller.TestController.* (..))")
+    public Object calculatePerformanceTime(ProceedingJoinPoint proceedingJoinPoint) {
+        Object result = null;
+        try {
+            long start = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
 
+            System.out.println("수행 시간 : "+ (end - start));
+        } catch (Throwable throwable) {
+            System.out.println("exception! ");
+        }
+        return result;
+    }
 }
+
